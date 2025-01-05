9275d90b68a06d8e783305aaf07627fa3c37654e
Cory Tomlinson <corytomlinson6640@gmail.com>
2025-01-05 13:06:44 -0700

    added sample trailer data

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,70 @@ import './App.css'
 function App() {
 
    interface Trailer {
-      length: number,
-      kingpinOffsetFromNose: number,
-      axlePositionFromNose: number, //midpoint between rear tandems or spread
-      axleSpread: number,
-      load: Array<Row>,
+      interiorLength: number,
+      kingpinDistanceFromNose: number,
+      tandemCenterDistanceFromNose: number,
+      tandemSpreadWidth: number,
+      loadRows: Array<Row>,
    }
 
-   type Row = [Position] | [Position|null,Position|null] //center OR both L + R
+   type Row = {_ctr_: Position} | {l___: Position|null, ___r: Position|null}
 
    interface Position {
-      frontEdgeDistanceFromNose: number, //feet
-      orientation: PalletOrientation,
+      /* front edge distance in feet from the nose */
+      depth: number,
+      /* orientation of pallet (stack) */
+      orien: O,
+      /* stack of arbitrary number of pallets. array index corresponds to position off the ground, i.e. bottom/single is 0, first stacked pallet is 1, and so on */
       stack: Array<Pallet>,
    }
 
    interface Pallet {
-      productWeight: number,
-      palletWeight: PalletWeight,
+      /* product weight in pounds */
+      prdWt: number,
+      /* pallet weight in pounds */
+      palWt: P,
    }
 
-   enum PalletOrientation {
-      Straight = 4.0, //feet
-      Sideways = 3.5, //feet
+   /* Orientation of pallet. Value is the corresponding length in feet */
+   enum O {
+      Straight = 4.0,
+      Sideways = 3.5,
    }
 
-   enum PalletWeight {
-      Chep = 60,  //pounds
-      White = 40, //pounds
+   /* Pallet color. Value is the corresponding weight in pounds */
+   enum P {
+      Chep = 60,
+      White = 40,
    }
 
 
    const [zoom,setZoom] = useState(1.0)
 
+   const sampleTrailer:Trailer = {
+      interiorLength: 53,
+      kingpinDistanceFromNose: 4,
+      tandemCenterDistanceFromNose: 40,
+      tandemSpreadWidth: 5,
+      loadRows: [
+         {l___: {depth: 0.0, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}, ___r: {depth: 0.0, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}},
+         {l___: {depth: 3.5, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}, ___r: {depth: 3.5, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}},
+         {l___: {depth: 7.0, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}, ___r: {depth: 7.0, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}},
+         {l___: {depth: 10.5, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}, ___r: {depth: 10.5, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}},
 
+         {l___: {depth: 14.0, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}, ___r: {depth: 14.0, orien: O.Sideways, stack: [{prdWt: 720, palWt: P.Chep}, {prdWt: 720, palWt: P.Chep}]}},
+         {l___: {depth: 17.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 17.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+         {l___: {depth: 21.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 21.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+         {l___: {depth: 24.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 24.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+
+         {l___: {depth: 28.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 28.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+         {l___: {depth: 31.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 31.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+         {l___: {depth: 35.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 35.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+         {l___: {depth: 38.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 38.5, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}},
+
+         {l___: {depth: 42.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}, ___r: {depth: 42.0, orien: O.Sideways, stack: [{prdWt: 1560, palWt: P.Chep}]}}
+      ]
+   }
 
    useEffect(() => {
       let canvas:HTMLCanvasElement = document.getElementById("load-diagram")! as HTMLCanvasElement
